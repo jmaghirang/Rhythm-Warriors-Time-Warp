fcac0286fe5e0b3891864cd50eba9d981d68b062
jamielee-lee <jamlee@nevada.unr.edu>
2023-12-03 20:08:09 -0800

    some scripts added + more ui assets

---
+++ Rhythm Warriors Time Warp/Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    // Class to manage enemies if we plan on putting different enemies in a level
+    public static EnemyManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    // ------------------------------------------------------------
+    // Defining attributes
+    // ------------------------------------------------------------
+
+
+    
+
+
+    // ------------------------------------------------------------
+    // Methods
+    // ------------------------------------------------------------
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Rhythm Warriors Time Warp/Assets/Scripts/Item/Item.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // ------------------------------------------------------------
+    // Defining attributes
+    // ------------------------------------------------------------
+
+
+    public string itemName;
+    public Sprite image;
+    public string itemDescription;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
