e1ea8736270d2ce4cc11f5c4984ef7ef580e029d
jmaghirang <70534153+jmaghirang@users.noreply.github.com>
2024-02-16 16:31:31 -0800

    UI toggle progress, still needs to be fixed

---
+++ Rhythm Warriors Time Warp/Assets/Scripts/UI/VRUIOperations.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// some code referenced from: 
+// https://www.youtube.com/watch?v=wVD4BKILJDs
+
+public class VRUIOperations : MonoBehaviour
+{
+    public UnityEvent OnEnter;
+    public UnityEvent OnExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TrigExit.instance.currentCollder = GetComponent<VRUIOperations>();
+        OnEnter.Invoke();
+    }
+}
