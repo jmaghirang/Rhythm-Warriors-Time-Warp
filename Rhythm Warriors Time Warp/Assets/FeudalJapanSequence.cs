2ec28b44acebd82eaf44c5f624fe7e4795ff058f
jamielee-lee <jamlee@nevada.unr.edu>
2024-03-07 17:58:08 -0800

    file and scene cleanup

---
+++ Rhythm Warriors Time Warp/Assets/FeudalJapanSequence.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FeudalJapanSequence : MonoBehaviour
+{
+    public Fragment fragment;
+
+    [SerializeField]
+    private bool trialComplete = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fragment.ID = Artifact.instance.fragments[2].ID;
+
+        SongManager.instance.StartSong();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!SongManager.instance.audioSource.isPlaying && !GameManager.instance.isPaused)
+        {
+            DialogueManager.instance.dialogueBox.UI.SetActive(true);
+            trialComplete = true;
+        }
+
+        if (trialComplete)
+        {
+            StartCoroutine(CollectFragment());
+        }
+
+        if (fragment.isCollected)
+        {
+            Artifact.instance.fragments[2].isCollected = fragment.isCollected;
+            SceneTransitionManager.instance.LoadNextScene((int)SceneIndexes.FEUDAL_JAPAN); //Next level - greece?
+        }
+    }
+
+    IEnumerator CollectFragment()
+    {
+        //ControlManager.instance.leftController.enabled = true;
+        ControlManager.instance.rightController.enabled = true;
+
+        DialogueManager.instance.dialogueBox.UI.SetActive(false);
+
+        fragment.gameObject.SetActive(true);
+
+        yield return new WaitUntil(() => fragment.isCollected);
+    }
+}
