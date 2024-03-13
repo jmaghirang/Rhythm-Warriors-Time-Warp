47f11da0a695ccc2cdfce24b95dcb509240cdac9
jamielee-lee <jamlee@nevada.unr.edu>
2024-03-13 16:27:23 -0700

    tuning level select

--- Rhythm Warriors Time Warp/Assets/Scripts/Scene Management/LevelSelector.cs
+++ Rhythm Warriors Time Warp/Assets/Scripts/Scene Management/LevelSelector.cs
@@ -52,7 +52,7 @@ public class LevelSelector : MonoBehaviour
         if (selectedLevels[0])
         {
             SetLevel("westSelected");
-            SetTitles(levelTitles[0].gameObject);
+            StartCoroutine(SetTitles(levelTitles[0].gameObject));
 
             levelText.text = "The Wild West";
         }
@@ -60,7 +60,7 @@ public class LevelSelector : MonoBehaviour
         else if (selectedLevels[1])
         {
             SetLevel("japanSelected");
-            SetTitles(levelTitles[1].gameObject);
+            StartCoroutine(SetTitles(levelTitles[1].gameObject));
 
             levelText.text = "Feudal Japan";
         }
@@ -68,7 +68,7 @@ public class LevelSelector : MonoBehaviour
         else if (selectedLevels[2])
         {
             SetLevel("greeceSelected");
-            SetTitles(levelTitles[2].gameObject);
+            StartCoroutine(SetTitles(levelTitles[2].gameObject));
 
             levelText.text = "Ancient Greece";
         }
@@ -76,7 +76,7 @@ public class LevelSelector : MonoBehaviour
         else if (selectedLevels[3])
         {
             SetLevel("egyptSelected");
-            SetTitles(levelTitles[3].gameObject);
+            StartCoroutine(SetTitles(levelTitles[3].gameObject));
 
             levelText.text = "Ancient Egypt";
         }
@@ -84,7 +84,7 @@ public class LevelSelector : MonoBehaviour
         else if (selectedLevels[4])
         {
             SetLevel("futureSelected");
-            SetTitles(levelTitles[4].gameObject);
+            StartCoroutine(SetTitles(levelTitles[4].gameObject));
 
             levelText.text = "The Future";
         }
@@ -98,14 +98,31 @@ public class LevelSelector : MonoBehaviour
         }
     }
 
-    public void SetTitles(GameObject activeTitle)
+    IEnumerator SetTitles(GameObject activeTitle)
     {
         foreach (Canvas c in levelTitles)
         {
-            c.gameObject.SetActive(false);
+            if (c.gameObject != activeTitle && c.gameObject.activeInHierarchy)
+            {
+                for (float i = 1; i >= 0; i -= Time.deltaTime)
+                {
+                    Image titleImage = c.GetComponentInChildren<Image>();
+                    titleImage.color = new Color(1, 1, 1, i);
+                    yield return null;
+                }
+
+                c.gameObject.SetActive(false);
+            }
         }
 
         activeTitle.SetActive(true);
+
+        for (float i = 0; i <=1; i += Time.deltaTime)
+        {
+            Image activeTitleImage = activeTitle.GetComponentInChildren<Image>();
+            activeTitleImage.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
     }
 
     public void ResetTriggers()
@@ -125,8 +142,16 @@ public class LevelSelector : MonoBehaviour
         levelSelectAnimator.SetTrigger(trigger);
     }
 
+    IEnumerator ButtonDelay(Button b)
+    {
+        yield return new WaitForSeconds(2f);
+        b.interactable = true;
+    }
+
     public void NextLevel(List<bool> levels)
     {
+        StopAllCoroutines();
+
         ResetSelectedLevels();
 
         if (i >= 4)
@@ -139,10 +164,15 @@ public class LevelSelector : MonoBehaviour
             levels[i + 1] = true;
             i++;
         }
+
+        next.interactable = false;
+        StartCoroutine(ButtonDelay(next));
     }
 
     public void PrevLevel(List<bool> levels)
     {
+        StopAllCoroutines();
+
         ResetSelectedLevels();
 
         if (i <= 0)
@@ -155,7 +185,93 @@ public class LevelSelector : MonoBehaviour
             levels[i - 1] = true;
             i--;
         }
+
+        prev.interactable = false;
+        StartCoroutine(ButtonDelay(prev));
     }
+
+    // some code from https://youtu.be/1VXeyeLthdQ?si=EVq-RXS0WxIY4Dkz
+    /*IEnumerator PlayPreview(AudioSource preview)
+    {  
+        float timeToFade = 1.25f;
+        float timeElapsed = 0;
+
+        preview.Play();
+
+        if (preview.clip != wildWestPreview.clip && wildWestPreview.isPlaying)
+        {
+            while (timeElapsed < timeToFade)
+            {
+                preview.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                wildWestPreview.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+
+                timeElapsed += Time.deltaTime;
+
+                yield return null;
+            }
+            
+            wildWestPreview.Stop();
+        }
+
+        else if (preview.clip != japanPreview.clip && japanPreview.isPlaying)
+        {   
+            while (timeElapsed < timeToFade)
+            {
+                preview.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                japanPreview.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+
+                timeElapsed += Time.deltaTime;
+
+                yield return null;
+            }
+
+            japanPreview.Stop();
+        }
+
+        else if (preview.clip != greecePreview.clip && greecePreview.isPlaying)
+        {
+            while (timeElapsed < timeToFade)
+            {
+                preview.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                greecePreview.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+
+                timeElapsed += Time.deltaTime;
+
+                yield return null;
+            }
+
+            greecePreview.Stop();
+        }
+
+        else if (preview.clip != egyptPreview.clip && egyptPreview.isPlaying)
+        {
+            while (timeElapsed < timeToFade)
+            {
+                preview.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                egyptPreview.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+
+                timeElapsed += Time.deltaTime;
+
+                yield return null;
+            }
+            egyptPreview.Stop();
+        }
+
+        else if (preview.clip != futurePreview.clip && futurePreview.isPlaying)
+        {
+            while (timeElapsed < timeToFade)
+            {
+                preview.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                futurePreview.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+
+                timeElapsed += Time.deltaTime;
+
+                yield return null;
+            }
+
+            futurePreview.Stop();
+        }
+    }*/
 
     public void GoToLevel()
     {
